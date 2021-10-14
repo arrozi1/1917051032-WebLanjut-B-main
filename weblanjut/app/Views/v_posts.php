c3e075270edcf8db2d179593e6c43d8ac7a0a061
Arrozi <you@example.com>
2021-10-14 13:56:25 +0700

    first commit

---
+++ weblanjut/app/Views/v_posts.php
@@ -0,0 +1,78 @@
+<div class="container mt-3">
+    <div class="text-center">
+        <h1>All Posts</h1>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-6 ">
+            <form action="" method="GET">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Cari postingan" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row mt-4">
+        <div class="col-md-4 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="https://source.unsplash.com/1600x900/?nature" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Nature</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="https://source.unsplash.com/1600x900/?programming" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Programming</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="https://source.unsplash.com/1600x900/?cat" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Cat</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="https://source.unsplash.com/1600x900/?sports" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Sport</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="https://source.unsplash.com/1600x900/?lifestyle" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Lifestyle</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card" style="width: 18rem;">
+                <img src="https://source.unsplash.com/1600x900/?technology" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Technology</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
